05bd9f9badf767f6b5211f25e6a9435c23cc6815
novel2430 <novel2430@163.com>
2025-04-10 14:02:53 +0800

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Mc Fabric Mod Development";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs }:
+  
+  let 
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    libs = with pkgs; [
+        libpulseaudio
+        libGL
+        glfw
+        openal
+        stdenv.cc.cc.lib
+      ];
+  in {
+    devShell.x86_64-linux = pkgs.mkShell {
+      packages = [];
+      buildInputs = libs;
+      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath libs;
+    };
+  };
+}
